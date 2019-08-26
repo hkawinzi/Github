f064752f22a27b0163c0c002ca96c0de41075ec8
happiness <happinesskawinzi@gmail.com>
2019-08-26 17:33:32 +0300

    adding styles to User component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import {User} from '../user';
+import {InfoRequestService} from '../info-http/info-request.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
+  providers:[InfoRequestService],
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  userName="";
+  user:User;
+  show=false;
+  show1=false;
+  repos=[];
+  showUser(){
+    this.show=true;
+    this.infoService.infoRequest(this.userName);
+    this.user=this.infoService.user;
+  }
+  showRepo(){
+    this.show1=true;
+    this.infoService.repoRequest(this.userName);
+    this.repos=this.infoService.repos;
+  }
+  // user= new User(0,"","",0,0,0);
+  constructor(private infoService:InfoRequestService) { }
 
   ngOnInit() {
   }
