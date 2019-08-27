e665c851034e560838c5fcd618f45231408a49f3
happiness <happinesskawinzi@gmail.com>
2019-08-27 04:03:22 +0300

    Add a function to search for repository by it's name

--- src/app/info-http/info-request.service.ts
+++ src/app/info-http/info-request.service.ts
@@ -9,8 +9,9 @@ export class InfoRequestService {
 
   user:User;
   repos=[];
+  repositories=[];
   constructor(private http:HttpClient) {
-    this.user= new User(0,"","",0,0,0);
+    this.user= new User(0,"","",0,0,0,"");
    }
    infoRequest(username:string){
      interface ApiResponse{
@@ -20,6 +21,7 @@ export class InfoRequestService {
        following:number;
        public_repos:number;
        repos_url:string;
+       created_at:string;
      }
      let promise = new Promise((resolve,reject)=>{
        this.http.get<ApiResponse>(environment.apiUrla+username+environment.apiUrlb).toPromise().then(response=>{
@@ -29,6 +31,7 @@ export class InfoRequestService {
          this.user.followers=response.followers;
          this.user.following=response.following;
          this.user.repositories=response.public_repos;
+         this.user.creationDate=response.created_at;
          resolve();
        },error=>{
          reject(error);
@@ -36,6 +39,7 @@ export class InfoRequestService {
      })
      return promise;
    }
+  }
    repoRequest(username:string){
      interface ApiResponse2{
       name:string;
@@ -63,4 +67,32 @@ export class InfoRequestService {
     })
     return promise;
    }
+   repoNameRequest(reponame:string){
+    interface ApiResponse3{
+     name:string;
+     description:string;
+     language:string;
+     forks:number;
+     html_url:string;
+     items;
+   }
+   let promise = new Promise((resolve,reject)=>{
+     this.http.get<ApiResponse3>(environment.apiUrl3a+reponame+environment.apiUrl3b).toPromise().then(response=>{
+       for(let i=0;i<response.items["length"];i++){
+         let newRepo= new Repository(0,"","","",0,"");
+         newRepo.repoId = i+1;
+         newRepo.repoName = response.items[i].name;
+         newRepo.description = response.items[i].description;
+         newRepo.language = response.items[i].language;
+         newRepo.forks = response.items[i].forks;
+         newRepo.url = response.items[i].html_url;
+         this.repositories.push(newRepo);
+       }
+       resolve();
+     },error=>{
+       reject(error);
+     })
+   })
+   return promise;
+  }
 }
